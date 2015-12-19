5da46b9be4b1e2952a3192bf5dca8e2e51341700
Mike Atkisson <mpatkisson@gmail.com>
2015-12-19 04:26:06 -0500

    who knows at this point?

---
+++ src/scripts/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const React = require('react'),
+      ReactDOM = require('react-dom');
+
+require('babel/register');            
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('example')
+);
